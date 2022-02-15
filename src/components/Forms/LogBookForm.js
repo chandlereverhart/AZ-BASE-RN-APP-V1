ed626a3996be7bed0d53b24335b59457df0ddd15
Chandler Everhart <chandler@giantkiller.com>
2022-02-15 12:28:53 -0700

    fixed date bug

--- src/components/Forms/LogBookForm.js
+++ src/components/Forms/LogBookForm.js
@@ -22,13 +22,14 @@ const LogBookForm = (props) => {
   const jump = props.route?.params?.jump?.jump ?? {};
   const navigation = useNavigation();
   const dispatch = useDispatch();
-  const [date, setDate] = useState(jump.createdAt.seconds * 1000 || new Date());
+  const [date, setDate] = useState(
+    jump.createdAt?.seconds * 1000 || new Date()
+  );
   const [image, setImage] = useState(jump.photoUrl || null);
   const [file, setFile] = useState(null);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
-    console.log("SELECTED DATE", currentDate);
     setDate(currentDate);
   };
   const pickImage = async () => {
